5be79b5cfadf77748c90f85664bba23c96b395e7
Austin Gil <austin@stegosource.com>
2018-07-25 19:32:40 -0700

    Incorporate Github API for public repos

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="project-list">
     <ul>
-      <li v-for="project in projects" :key="project">
-        {{ project }}
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
       </li>
     </ul>
   </div>
@@ -11,8 +11,30 @@
 <script>
 export default {
   data: () => ({
-    projects: ["Project 1", "Project 2", "Project 3", "Project 4"]
-  })
+    projects: []
+  }),
+
+  async created() {
+    const username = "SDVue";
+    // Fetch Docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+    // Github API Docs: https://developer.github.com/v3/repos/#list-user-repositories
+
+    // Fetch the API response
+    this.projects = await fetch(
+      `https://api.github.com/users/${username}/repos`
+    ).then(
+      // Transform the response object into the response json (what we really want)
+      res => res.json()
+    );
+  }
+  // Example without async/await
+  // created() {
+  //   fetch(`https://api.github.com/users/${username}/repos`)
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       this.items = res;
+  //     });
+  // }
 };
 </script>
 
