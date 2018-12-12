aed9bb9ca67ff3f444c94920639bcaad16daec19
Austin Gil <austin@stegosource.com>
2018-12-11 20:28:34 -0800

    Passing tests with actual component

--- tests/unit/ProjectDetails.spec.js
+++ tests/unit/ProjectDetails.spec.js
@@ -4,7 +4,6 @@ import ProjectDetails from "@/views/ProjectDetails.vue";
 describe("ProjectDetails.vue", () => {
   it("does the tests!!!", () => {
     const wrapper = shallowMount(ProjectDetails);
-    console.log(wrapper);
-    expect(1 + 1).toEqual(2);
+    expect(wrapper.html()).toBeUndefined();
   });
 });
