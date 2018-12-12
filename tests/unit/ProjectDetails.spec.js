32ca0c2ea0b150d1d7e66934868af1ca82cbac38
Austin Gil <austin@stegosource.com>
2018-12-11 20:29:59 -0800

    Passing in props to test components

--- tests/unit/ProjectDetails.spec.js
+++ tests/unit/ProjectDetails.spec.js
@@ -3,7 +3,13 @@ import ProjectDetails from "@/views/ProjectDetails.vue";
 
 describe("ProjectDetails.vue", () => {
   it("does the tests!!!", () => {
-    const wrapper = shallowMount(ProjectDetails);
+    const wrapper = shallowMount(ProjectDetails, {
+      props: {
+        username: "VueSD",
+        project: "github-portfolio"
+      }
+    });
     expect(wrapper.html()).toBeUndefined();
+    expect(wrapper.vm.details).toBeNull();
   });
 });
