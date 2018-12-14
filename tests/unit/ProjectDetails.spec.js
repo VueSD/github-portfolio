23f3c284367d8cfab7b82a8afa49bab6110bb34e
Austin Gil <austin@stegosource.com>
2018-12-13 21:20:13 -0800

    Writing tests for vm.created()

--- tests/unit/ProjectDetails.spec.js
+++ tests/unit/ProjectDetails.spec.js
@@ -10,20 +10,54 @@ API.getProjectDetails = jest.fn(({ username, project }) => {
   });
 });
 
+const username = "VueSD";
+const project = "github-portfolio";
+
 describe("ProjectDetails.vue", () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallowMount(ProjectDetails, {
       stubs: ["router-link"],
       propsData: {
-        username: "VueSD",
-        project: "github-portfolio"
+        username,
+        project
       }
     });
   });
 
   describe("vm.created()", () => {
-    // Tests
+    it("should call the API with props data", () => {
+      expect(API.getProjectDetails).toHaveBeenCalledWith({
+        username,
+        project
+      });
+    });
+    it("should update vm.details", () => {
+      expect(wrapper.vm.details).toEqual({
+        name: `Project: ${project}`,
+        description: `A fancy project by ${username}.`,
+        html_url: `https://github.com/${username}/${project}`
+      });
+    });
+    it("should show new vm.details with new props", () => {
+      const newUsername = "vuejs";
+      const newProject = "vue";
+      const wrapper = shallowMount(ProjectDetails, {
+        stubs: ["router-link"],
+        propsData: {
+          username: newUsername,
+          project: newProject
+        }
+      });
+      wrapper.vm.$nextTick(() => {
+        expect(API.getProjectDetails).toHaveBeenCalled();
+        expect(wrapper.vm.details).toEqual({
+          name: `Project: ${newProject}`,
+          description: `A fancy project by ${newUsername}.`,
+          html_url: `https://github.com/${newUsername}/${newProject}`
+        });
+      });
+    });
   });
 
   describe("html", () => {
