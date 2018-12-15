c562d9060e595bbe3b9eb8a873e7f40731799284
Austin Gil <austin@stegosource.com>
2018-12-14 18:45:25 -0800

    Writing simple e2e test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,13 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.contains("h1", "GitHub Projects");
+    cy.get("a")
+      .first("a")
+      .click();
+    // cy.wait(1000);
+    cy.get("h3");
+    cy.get(".router-link-active").click();
+    cy.contains("h1", "GitHub Projects");
   });
 });
